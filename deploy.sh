e33bc89518a9cf2179352487f8390aa9d4a1c9a7
金辰浩 <jinchenhao@meituan.com>
2018-06-13 14:26:28 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,41 @@
 #~/bin/bash
 
+echo "deploying homepage"
+
+source ../config.sh
+
+# copy
+
+rm -rf dist/
+mkdir dist
+mkdir dist/static
+cp index.html dist/
+
+legalFormats=".jpg .css"
+for format in $legalFormats
+do  
+    cp -r static/*$format dist/static/
+done
+
+# compress jpgs
+
+cache="cache.jpg"
+
+all_jpgs=$(find dist/static/*.jpg)
+for img in $all_jpgs
+do {
+    echo "compressing $img"
+    rm -f $cache
+    guetzli --quality 90 $img $cache
+    mv -f $cache $img
+} &
+done
+wait
+
+# deploy to remote
+
+DEST="~/home/"
+
+echo "deploying to $DEST at $USER@$URL"
+
+rsync -r dist/* $USER@$URL:$DEST
